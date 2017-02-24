f4acc03f2d2bc9bf910dddcea15bc1138ed1e1f1
saltyx <shiyan233@hotmail.com>
2017-02-24 16:09:36 +0800

    initial commit

---
+++ src/Test.java
@@ -0,0 +1,112 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+import Trie.Trie;
+
+/**
+ * Created by shiyan on 2017/2/16.
+ * Contact: shiyan233@hotmail.com
+ *          saltyx.github.io
+ */
+public class Test {
+
+    public static void main(String[] argv) throws IOException {
+        Files.walkFileTree(Paths.get("./test"), new SimpleFileVisitor<Path>(){
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                info("Visiting test folder");
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (file.getFileName().toString().matches("^[^.,/]+.dic$")) {
+                    info("Visiting "+file.getFileName());
+                    Trie trie = new Trie();
+                    List<String> list = new ArrayList<>();
+                    Files.lines(file).distinct().forEach(list::add);
+                    long start = System.currentTimeMillis();
+                    trie.build(list);
+                    long end = System.currentTimeMillis();
+                    info("Reduced Trie has been built");
+                    info(String.format("Time Used: %d ms", end-start));
+                    info("Running all test cases");
+                    int passed = 0, failed = 0;
+                    long start1 = System.currentTimeMillis();
+                    for (String x: list) {
+                        if (trie.search(x)) {
+                            passed++;
+                        } else {
+                            failed++;
+                            error(x+" failed");
+                        }
+                    }
+                    long end1 = System.currentTimeMillis();
+                    info("=============search test result==============");
+                    if (list.size() > 1) {
+                        info("$Total : "+String.valueOf(list.size()) + " entries");
+                    } else {
+                        info("$Total 1 entry");
+                    }
+                    info(String.format("$Pass %d\t$Fail %d\t$Time Used %d ms", passed, failed, end1-start1));
+
+                    info("=============prefix test result==============");
+
+                    Scanner in = new Scanner(System.in);
+                    while (in.hasNext()) {
+                        String str = in.next();
+                        if (str.contentEquals("exit")) break;
+                        List<String> list1 = trie.findByPrefix(str);
+                        if (list1 != null) {
+                            list1.forEach(Test::hint);
+                        } else {
+                            info("no hint");
+                        }
+                    }
+                    info("=============delete test result==============");
+                    failed = passed = 0;
+                    for (String x: list) {
+                        if (trie.delete(x)) {
+                            if (trie.search(x)) {
+                                failed++;
+                            } else {
+                                passed++;
+                            }
+                        } else {
+                            failed++;
+                        }
+                    }
+                    if (list.size() > 1) {
+                        info("$Total : "+String.valueOf(list.size()) + " entries");
+                    } else {
+                        info("$Total 1 entry");
+                    }
+                    info(String.format("$Pass %d\t$Fail %d", passed, failed));
+                }
+                return FileVisitResult.CONTINUE;
+            }
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                return super.visitFileFailed(file, exc);
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                return super.postVisitDirectory(dir, exc);
+            }
+        });
+    }
+
+    private static void hint(String hint) {
+        System.out.println(String.format("[hint] %s", hint));
+    }
+
+    private static void info(String info) {
+        System.out.println(String.format("[info] %s", info));
+    }
+
+    private static void error(String err) {
+        System.err.println(String.format("[error] %s", err));
+    }
+}
---
+++ src/Trie/Trie.java
@@ -0,0 +1,436 @@
+package Trie;
+
+import java.util.*;
+
+/**
+ * Created by shiyan on 2017/2/16.
+ * Contact: shiyan233@hotmail.com
+ *          saltyx.github.io
+ */
+
+public class Trie {
+
+    private static int DEFAULT_SIZE = 4096 * 32;
+    private static int DEFAULT_TAIL_SIZE = 4096;
+    private static int END_FLAG = 1;
+    private static int DEBUG_MODE = 0;
+
+    private int [] base;
+    private int [] check;
+    private int [] tail;
+    private List<Integer> [] lists;
+
+    private int position;
+
+    public Trie() {
+        initial();
+    }
+
+    public Trie(int... size) {
+        initial(size);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initial(int... size) {
+        switch (size.length) {
+            case 0 :
+                base = new int[DEFAULT_SIZE];
+                check = new int[DEFAULT_SIZE];
+                lists = new List[DEFAULT_SIZE];
+                tail = new int[DEFAULT_TAIL_SIZE];
+                break;
+            case 1:
+                base = new int[size[0]];
+                check = new int[size[0]];
+                lists = new List[size[0]];
+                tail = new int[DEFAULT_TAIL_SIZE];
+                break;
+            case 2:
+                base = new int[size[0]];
+                check = new int[size[0]];
+                lists = new List[size[0]];
+                tail = new int[size[1]];
+                break;
+            default:
+                if (DEBUG_MODE == 1) {
+                    info("using default size");
+                }
+                base = new int[DEFAULT_SIZE];
+                check = new int[DEFAULT_SIZE];
+                lists = new List[DEFAULT_SIZE];
+                tail = new int[DEFAULT_TAIL_SIZE];
+                break;
+        }
+
+        base[1] = 1;
+        check[1] = 1;
+        position = 1;
+    }
+
+    public void build(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            int code = insert(list.get(i));
+            switch (code) {
+                case -1:
+                    error("accessing the empty node");
+                    return;
+                case -2:
+                    if (DEBUG_MODE == 1) {
+                        info(list.get(i) + " #"+ String.valueOf(i) + " has already been in the dictionary");
+                    }
+                    break;
+                default:
+                    if (DEBUG_MODE == 1) {
+                        info(list.get(i)+"#"+i);
+                    }
+                    break;
+            }
+        }
+    }
+
+    public int insert(String key) {
+        int[] keyValue = string2IntArray(key);
+        if (!search(keyValue)) {
+            int pre = 1;
+            for (int i=0; i<keyValue.length; i++) {
+                if (base[pre] < 0) {
+                    //if current node is an end-point ,then separate or create a new node
+                    int oldBase = base[pre];
+                    if (tail[-oldBase] == keyValue[i]) {
+                        //create a new node
+                        base[pre] = xCheck(keyValue[i]);
+
+                        checkBC(base[pre] + keyValue[i]);
+                        base[ base[pre]+keyValue[i] ] = oldBase;
+                        check[ base[pre]+keyValue[i] ] = pre;
+                        put(pre, keyValue[i]);
+                        moveTail(-oldBase);
+                        pre = base[pre] + keyValue[i];
+                    } else {
+                        //separate
+                        List<Integer> list = new ArrayList<>();
+                        list.add(tail[-oldBase]); list.add(keyValue[i]);
+                        base[pre] = xCheck(list);
+                        checkBC(base[pre] + tail[-oldBase]);
+                        checkBC(base[pre] + keyValue[i]);
+                        base[ base[pre]+tail[-oldBase] ] = oldBase;
+                        base[ base[pre]+keyValue[i] ] = -position;
+                        check[ base[pre]+tail[-oldBase] ] = check[ base[pre]+keyValue[i] ] = pre;
+                        writeTail(keyValue, i+1);
+                        put(pre, tail[-oldBase]);
+                        put(pre, keyValue[i]);
+                        moveTail(-oldBase);
+                        break;// 2 new nodes
+                    }
+                } else if (base[pre] > 0) {
+                    if (check[ base[pre] + keyValue[i] ] == 0) {
+                        check[ base[pre] + keyValue[i] ] = pre;
+                        base[ base[pre] + keyValue[i] ] = -position;
+                        put(pre, keyValue[i]);
+                        writeTail(keyValue, i + 1);
+                        break;//a new node
+                    } else if (check[ base[pre] + keyValue[i] ] == pre) {
+                        //put(pre, keyValue[i]);
+                        pre = base[pre] + keyValue[i];
+                    } else {
+                        processConflict(pre, check[ base[pre] + keyValue[i] ], keyValue[i]);
+                        writeTail(keyValue, i + 1);
+                        break;//a new node
+                    }
+                } else return -1;
+            }
+        } else {
+            return -2;
+        }
+        return 0;
+    }
+
+    private int processConflict(int node1, int node2, int newNodeValue) {
+        int node = (lists[node1].size()+1) < lists[node2].size() ? node1 : node2;
+        int oldNodeBase = base[node];
+        if (node == node1) {
+            base[node] = xCheck(lists[node], newNodeValue);
+        } else {
+            base[node] = xCheck(lists[node]);
+        }
+        for (int i = 0; i < lists[node].size(); i++) {
+            int oldNext = oldNodeBase + lists[node].get(i);
+            int newNext = base[node] + lists[node].get(i);
+            if (oldNext == node1) node1 = newNext;
+            base[newNext] = base[oldNext];
+            check[newNext] = node;
+            if (base[oldNext] > 0) {
+                for (int j = 0; j < lists[oldNext].size(); j++) {
+                    check[ base[oldNext] + lists[oldNext].get(j) ] = newNext;
+                    put(newNext, lists[oldNext].get(j));
+                }
+                lists[oldNext] = null;
+            }
+            base[oldNext] = 0; check[oldNext] = 0;
+        }
+        base[ base[node1] + newNodeValue ] = -position;
+        check[ base[node1] + newNodeValue ] = node1;
+        put(node1, newNodeValue);
+        return node;
+    }
+
+    private void checkBC(int p) {
+        if (p >= base.length) {
+            extendBC();
+        }
+    }
+
+    private void checkTail(int p) {
+        if (p >= tail.length) {
+            extendT();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void extendBC() {
+        int[] base1 = new int[base.length + DEFAULT_SIZE];
+        int[] check1 = new int[check.length + DEFAULT_SIZE];
+        List<Integer>[] lists1 = new List[lists.length + DEFAULT_SIZE];
+        System.arraycopy(base, 0, base1, 0, base.length);
+        System.arraycopy(check, 0, check1, 0, check.length);
+        System.arraycopy(lists, 0, lists1, 0, lists.length);
+        base = base1;
+        check = check1;
+        lists = lists1;
+    }
+
+    private void extendT() {
+        int[] tail1 = new int[tail.length + DEFAULT_TAIL_SIZE];
+        System.arraycopy(tail, 0, tail1, 0 ,tail.length);
+        tail = tail1;
+    }
+
+    private boolean search(int[] key) {
+        int pre = 1;
+        for (int i = 0; i < key.length; i++) {
+            if (base[pre] < 0) {
+                return compareTail(-base[pre], i, key);
+            } else if (base[pre] > 0) {
+                checkBC(base[pre]+key[i]);
+                if (check[ base[pre] + key[i] ] == pre) {
+                    pre = base[pre] + key[i];
+                } else {
+                    return false;
+                }
+            } else return false;
+        }
+        return true;
+    }
+
+    public boolean search(String key) {
+        int[] keyValue = string2IntArray(key);
+        return search(keyValue);
+    }
+
+    private boolean compareTail(int start,int keyIndex, int[] key) {
+        checkTail(start+key.length-keyIndex+1);
+        for (int i = start, j=keyIndex; j < key.length ; i++,j++) {
+            if (key[j] != tail[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void moveTail(int start) {
+        moveTail(start, 1);
+    }
+
+    private void moveTail(int start, int steps) {
+        if (steps <= 0 || tail[start] == END_FLAG) return;
+        int i= start + steps;
+        for (;tail[i] != END_FLAG; i++) {
+            tail[i - steps] = tail[i];
+        }
+        for (int j = 0; j < steps; j++,i--) {
+            tail[i] = 0;
+        }
+        tail[i] = END_FLAG;
+    }
+
+    public boolean delete(String key) {
+        int []keyValue = string2IntArray(key);
+        int pre = 1;
+        int index = -1;
+        int tempVal;
+        int next;
+        do {
+            index++;
+            tempVal = keyValue[index];
+            next = base[pre] + tempVal;
+            if (check[next] != pre)  {
+                return false;
+            }
+            if (base[next] < 0) break;
+            pre = next;
+        } while (true);
+        if (tempVal == END_FLAG || compareTail(-base[next], index+1, keyValue)) {
+            for (int i = 0; i < lists[pre].size(); i++) {
+                if (lists[pre].get(i) == tempVal) {
+                    lists[pre].remove(i);break;
+                }
+            }
+            base[next] = 0; check[next] = 0;
+            //info(String.format("%s next[%d] turn to 0",key, next));
+            return true;
+        }
+        return false;
+    }
+
+    public List<String> findByPrefix(String prefix) {
+        StringBuilder builder = new StringBuilder();
+        char[] key = prefix.toCharArray();
+        int pre = 1;
+        int next = 1;
+        int index = 0;
+        char tempVal;
+
+        while (index < key.length) {
+            tempVal = key[index];
+            next = base[pre] + tempVal;
+            builder.append(tempVal);
+            ++index;
+            if (check[next] != pre) return null;
+            if (base[next] < 0) break;
+            pre = next;
+
+        }
+
+        List<String> list = new ArrayList<>();
+
+        if (base[next] < 0) {
+            int i = 0;
+            while (index < key.length) {
+                if (tail[-base[next] + i] == key[index]) {
+                    builder.append((char) tail[-base[next]+i]);
+                    ++i;
+                    ++index;
+                }
+                else return null;
+            }
+            while (tail[-base[next]+i] != END_FLAG) {
+                builder.append((char) tail[-base[next]+i]);
+                ++i;
+            }
+            list.add(builder.toString());
+            return list;
+        } else {
+            List<String> temp = new LinkedList<>();
+            find(pre, new StringBuilder(), temp);
+            for (String x: temp) {
+                list.add(builder.toString() + x);
+            }
+            return list;
+        }
+    }
+
+    private void find( int pre, StringBuilder builder, List<String> list) {
+        int next;
+        if (base[pre] < 0) {
+            builder.append(readTail(-base[pre]));
+            list.add(builder.toString());
+            return;
+        }
+        for (int i = 0; i < lists[pre].size(); i++) {
+            next = base[pre] + lists[pre].get(i);
+            StringBuilder reserved = new StringBuilder(builder);
+            if (check[next] == pre) {
+                find(next, builder.append((char) lists[pre].get(i).intValue()), list);
+            }
+            builder = reserved;
+        }
+    }
+
+    private String readTail(int start) {
+        StringBuilder builder = new StringBuilder();
+        while (tail[start] != END_FLAG) {
+            builder.append((char) tail[start++]);
+        }
+        return builder.toString();
+    }
+
+    private int xCheck(int x) {
+        int q = 1;
+        while (true) {
+            if (check[q+x] != 0) {
+                q++;
+            } else {
+                break;
+            }
+        }
+        return q;
+    }
+
+    private int xCheck(List<Integer> list) {
+        int q = 1;
+        findQ:while (true) {
+            for (int i = 0; i < list.size(); i++) {
+                if (check[q+list.get(i)] != 0) {
+                    q++;
+                    continue findQ;
+                }
+            }
+            break;
+        }
+        return q;
+    }
+
+    private int xCheck(List<Integer> list, int x) {
+        int q = 1;
+        findQ:while (true) {
+            for (int i = 0; i < list.size(); i++) {
+                if (check[q+list.get(i)] != 0 || check[q+x] != 0) {
+                    q++;
+                    continue findQ;
+                }
+            }
+            break;
+        }
+        return q;
+    }
+
+    private void writeTail(int[] key, int start) {
+        if (start >= key.length) return;
+        checkTail(position+key.length-start+1);
+        for (int i=position, j=start; j<key.length; position++,i++,j++) {
+            tail[i] = key[j];
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void put(int key, int value) {
+        if (lists[key] == null) {
+            lists[key] = new LinkedList();
+            lists[key].add(value);
+        } else {
+            lists[key].add(value);
+        }
+    }
+
+    private int[] string2IntArray(String key) {
+        if (key == null || key.length() == 0) return null;
+        int[] result = new int[key.length()+1];
+        for (int i=0; i<key.length(); i++) {
+            result[i] = key.charAt(i);
+        }
+        result[key.length()] = END_FLAG;
+        return result;
+    }
+
+    private void log(String key, Object value) {
+        System.out.println(String.format("%s : %s",key, String.valueOf(value)));
+    }
+
+    private void error(String value) {
+        System.err.println(String.format("[error] %s", value));
+    }
+
+    private void info(String value) {
+        System.out.println(String.format("[info] %s", value));
+    }
+}
